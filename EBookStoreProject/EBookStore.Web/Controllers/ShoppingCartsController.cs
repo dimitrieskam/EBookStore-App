e9f2b5e76b679814140ab8baaaa41734fe3e001e
dimitrieskam <marija.dimitrieska@students.finki.ukim.mk>
2025-06-29 18:50:34 +0200

    Dockerfile and docker-compose

---
+++ EBookStoreProject/EBookStore.Web/Controllers/ShoppingCartsController.cs
@@ -0,0 +1,80 @@
+﻿using EBookStore.Service.Interface;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using Stripe;
+namespace EBookStore.Web.Controllers
+{
+    public class ShoppingCartsController : Controller
+    {
+        private readonly IShoppingCartService _shoppingCartService;
+
+        private readonly IConfiguration _configuration;
+
+        public ShoppingCartsController(IShoppingCartService shoppingCartService, IConfiguration configuration)
+        {
+            _shoppingCartService = shoppingCartService;
+            _configuration = configuration;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier) ?? null;
+
+            return View(_shoppingCartService.getShoppingCartDetails(userId ?? ""));
+        }
+        public async Task<IActionResult> DeleteProductFromShoppingCart(Guid? productId)
+        {
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier) ?? null;
+
+            var result = _shoppingCartService.deleteFromShoppingCart(userId, productId);
+
+            return RedirectToAction("Index", "ShoppingCarts");
+        }
+        public async Task<IActionResult> Order()
+        {
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier) ?? null;
+
+            var result = _shoppingCartService.orderProducts(userId ?? "");
+
+            return RedirectToAction("Index", "ShoppingCarts");
+        }
+        public IActionResult SuccessPayment()
+        {
+            return View();
+        }
+
+        public IActionResult PayOrder(string stripeEmail, string stripeToken)
+        {
+            StripeConfiguration.ApiKey = _configuration["Stripe:SecretKey"];
+            var customerService = new CustomerService();
+            var chargeService = new ChargeService();
+            string userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            var order = this._shoppingCartService.getShoppingCartDetails(userId);
+
+            var customer = customerService.Create(new CustomerCreateOptions
+            {
+                Email = stripeEmail,
+                Source = stripeToken
+            });
+
+            var charge = chargeService.Create(new ChargeCreateOptions
+            {
+                Amount = (Convert.ToInt32(order.TotalPrice) * 100),
+                Description = "EShop Application Payment",
+                Currency = "usd",
+                Customer = customer.Id
+            });
+
+            if (charge.Status == "succeeded")
+            {
+                this.Order();
+                return RedirectToAction("SuccessPayment");
+
+            }
+            else
+            {
+                return RedirectToAction("NotsuccessPayment");
+            }
+        }
+    }
+}
